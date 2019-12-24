3b26e56b0b30350f5fd42b733973379359a822bb
tiny <343014044@qq.com>
2019-12-24 21:05:51 +0800

    Initial commit

---
+++ src/main/java/SortUtils.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class SortUtils {
+
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+        Random random = new Random();
+        for (int i = 0; i < 10; i++) {
+            arr[i] = random.nextInt(20);
+        }
+        System.out.println(Arrays.toString(arr));
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - i - 1; i++) {
+            for (int j = i; j < arr.length - 2; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    arr[j] = arr[j] + arr[j + 1];
+                    arr[j + 1] = arr[j] - arr[j + 1];
+                    arr[j] = arr[j] - arr[j + 1];
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/StudentNamespaceHandler.java
@@ -0,0 +1,11 @@
+
+import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
+
+public class StudentNamespaceHandler extends NamespaceHandlerSupport {
+
+    @Override
+    public void init() {
+        registerBeanDefinitionParser("student", new StudentBeanDefinitionParser());
+    }
+
+}
